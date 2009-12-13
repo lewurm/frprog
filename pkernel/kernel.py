3140c0cc070989e69eff0bff76dde4d00f2a1940
Bernhard Urban <lewurm@gmx.net>
2009-12-13 22:37:13 +0100

    prep for python kernelprog

--- pkernel/kernel.py
+++ pkernel/kernel.py
@@ -7,130 +7,74 @@ DEVICE="/dev/ttyUSB0"
 # baudrate used for communication with pkernel
 KERNEL_BAUDRATE=38400
 
+def pkernWRITE(address, size, data):
+	# send WRITE command
+	sendByte(0x01)
+	if (recvByte() != 0xF1):
+		raise Exception
+	sendByte(0x03)
+	if (recvByte() != 0x83):
+		raise Exception
+	# tell desired address and size
+	sendDWord(address)
+	sendWord(size)
+	# write binary stream of data
+	for i in range(0, size):
+		sendByte(data[i])
+	# get checksum
+	recvChecksum()
+
 print "Initializing serial port..."
-tty = SerialPort(DEVICE, 500, INIT_BAUDRATE)
+tty = SerialPort(DEVICE, 500, KERNEL_BAUDRATE)
 
-while 1:
-	tty.write('V')
-	tty.flush()
-	try: 
-		if tty.read() == 'F':
-			break
-	except SerialPortException: 
-		# timeout happened, who cares ;-)
-		pass
+# check command line arguments
+if len(sys.argv) != 2:
+	print "Usage: " + sys.argv[0] + " [mhx-file]"
+	sys.exit(1)
 
-print "OK, trying to set baudrate..."
-# set baudrate
-cmdBAUDRATE(REAL_BAUDRATE)
-tty = SerialPort(DEVICE, 100, REAL_BAUDRATE)
+# read in data from mhx-file before starting
+try:
+	fp = open(sys.argv[1], "r")
+except IOError:
+	print sys.argv[0] + ": Error - couldn't open file " + sys.argv[1] + "!"
+	sys.exit(1)
 
-"""
-print
-sendByte(0x01)
-print recvByte()
-sendByte(0x02)
-print recvByte()
-sys.exit(0)
-"""
-"""
-# execute (existing) program in ram
-cmdCALL(0x00033ffc)
-sys.exit(0)
-"""
+linecount = 0
+for line in fp:
+	linecount += 1
+	# get rid of newline characters
+	line = line.strip()
 
-# read something from the IRAM
-#print cmdREAD(0x00030000, 32)
+	# we're only interested in S2 (data sequence with 3 address bytes) records by now
+	if line[0:2] == "S2":
+		byte_count = int(line[2:4], 16)
+		# just to get sure, check if byte count field is valid
+		if (len(line)-4) != (byte_count*2):
+			print sys.argv[0] + ": Warning - inavlid byte count field in " + \
+				sys.argv[1] + ":" + str(linecount) + ", skipping line!"
+			continue
 
-#data = []
-#for i in range(0, 32):
-#	data.append(i)
-#cmdWRITE(0x00030000, 32, data)
+		# address and checksum bytes are not needed
+		byte_count -= 4
+		address = int(line[4:10], 16)
+		datastr = line[10:10+byte_count*2]
 
+		# convert data hex-byte-string to real byte data list
+		data = []
+		for i in range(0, len(datastr)/2):
+			data.append(int(datastr[2*i:2*i+2], 16))
 
-"""
-# write something to the begin of the IRAM
-data_wr = []
-checksum = 0
-for i in range(0, 0x400):
-	value = i%256
-	data_wr.append(value)
-	checksum = (checksum + value) % (2**16)
+		# add flash sequence to our list
+		flashseqs.append(FlashSequence(address, data))
 
-print "Calculated checksum:", checksum
-print "Writing", data_wr, "to the IRAM..."
-cmdWRITE(0x00030000, len(data_wr), data_wr)
-print "Received Checksum:", last_checksum
-print
-
-print "Reading from the IRAM again..."
-data_re = cmdREAD(0x00030000, len(data_wr))
-print "Received data:", data_re, "Checksum:", last_checksum
-"""
-
-"""
-# see whats in the iram at the moment
-data_wr = []
-print "Reading from the IRAM..."
-data_re = cmdREAD(0x00030000, 0x10000-4)
-print "Received data:", data_re, "Checksum:", last_checksum
-"""
-
-"""
-# see whats in the dram at the moment
-data_wr = []
-print "Reading from the DRAM..."
-data_re = cmdREAD(0x0002C000, 0x10000-0xC000-4)
-print "Received data:", data_re, "Checksum:", last_checksum
-"""
-
-"""
-# blank the iram
-data_wr = []
-for i in range(0, 0x10000-4):
-	value = 0
-	data_wr.append(value)
-
-print "Writing", data_wr, "to the IRAM..."
-cmdWRITE(0x00030000, len(data_wr), data_wr)
-print "Received Checksum:", last_checksum
-print
-"""
-
-"""
-# blank the dram
-data_wr = []
-for i in range(0, 0x10000-0xC000-4):
-	value = 0
-	data_wr.append(value)
-
-print "Writing", data_wr, "to the DRAM..."
-cmdWRITE(0x0002C000, len(data_wr), data_wr)
-print "Received Checksum:", last_checksum
-print
-"""
+print "The following flash sequences have been read in:"
+for seq in flashseqs:
+	print hex(seq.address) + ":", seq.data
 
 
-# write some data in the iram and try to execute it
-data_wr =[
-		0x9B,0x00,
-		0x0D,0x4e,
-		0xcf,0xf1,
-		0x16,0x01,
-		0x9b,0x05,
-		0x04,0xc7,
-		0xc1,0x06,
-		0x16,0x56,
-		0xe0,0xfb, #branch
-		0x9f,0xa0,0x9f,0xa0,0x9f,0xa0, #nop
-		0x9f,0xa0,0x9f,0xa0,0x9f,0xa0,
-		0x9f,0xa0,0x9f,0xa0,0x9f,0xa0,
-		0x9f,0xa0,0x9f,0xa0,0x9f,0xa0,
-		0x9f,0xa0,0x9f,0xa0,0x9f,0xa0,
-		0x9f,0xa0,0x9f,0xa0,0x9f,0xa0,
-		0x9f,0xa0,0x9f,0xa0,0x9f,0xa0]
-print "Writing", data_wr, "to the IRAM..."
-cmdWRITE(0x00030000, len(data_wr), data_wr)
-print "Received Checksum:", last_checksum
-print
-cmdCALL(0x00030000)
+# let the fun begin!
+for seq in flashseqs:
+	print "Flashing", len(seq.data), "bytes at address", hex(seq.address)
+	pkernWRITE(seq.address, len(seq.data), seq.data)
+
+print "Reset your board now to run code from Flash"
